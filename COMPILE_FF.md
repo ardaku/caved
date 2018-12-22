ba171ca69cc28ac2fcddd8e2c52ad659fcda16f8
Jeron Lau <jeron.lau@plopgrizzly.com>
2018-12-21 18:00:22 -0600

    Got statically linked caving to work.

--- COMPILE_FF.md
+++ COMPILE_FF.md
@@ -2,7 +2,8 @@
 Use:
 
 ```
-./configure --prefix=fakeroot --cc=gcc\ -fPIC
+# ./configure --prefix=fakeroot --cc=gcc\ -fPIC
+./configure --prefix=fakeroot --enable-pic --disable-alsa --disable-sdl2
 ```
 
 to build for X86.
--- caving/build.rs
+++ caving/build.rs
@@ -1,21 +1,23 @@
 extern crate cc;
 
-// gcc ffplay.c cmdutils.c -lSDL2 -lm -lavcodec -lavdevice -lavutil -lavfilter -lswresample -lswscale -lavformat -I../
-
 fn main() {
     cc::Build::new()
-//        .warnings(false)
         .include("../ffmpeg-4.1")
         .file("ffplay.c")
         .file("cmdutils.c")
         .compile("ffplay");
 
-    println!("cargo:rustc-link-lib=avcodec");
-    println!("cargo:rustc-link-lib=avdevice");
-    println!("cargo:rustc-link-lib=avutil");
-    println!("cargo:rustc-link-lib=avfilter");
-    println!("cargo:rustc-link-lib=swresample");
-    println!("cargo:rustc-link-lib=swscale");
-    println!("cargo:rustc-link-lib=avformat");
     println!("cargo:rustc-link-lib=SDL2");
+    println!("cargo:rustc-link-search=src/x86_64");
+
+    // FFMPEG Static Libraries:
+    println!("cargo:rustc-link-lib=static=avformat"); // -lavformat -lm -lbz2 -lz
+        println!("cargo:rustc-link-lib=static=avcodec"); // -lavcodec -pthread -lm -lz
+            println!("cargo:rustc-link-lib=static=swresample"); // -lswresample -lm
+        println!("cargo:rustc-link-lib=bz2");
+        println!("cargo:rustc-link-lib=z");
+
+    // TODO: Use Vulkan/OpenGL scaling instead of this library.
+    println!("cargo:rustc-link-lib=static=swscale"); // -lswscale -lm 
+        println!("cargo:rustc-link-lib=static=avutil"); // -lavutil -pthread -lm
 }
